463e30cd8e229e87c2b23d5a128ee481397f8e22
Damian Reiter <damian.reiter@gmail.com>
2017-10-21 18:19:51 -0400

    fixed view engine

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var express = require('express'),
 
 mongoose.connect(process.env.DB_URL, {useMongoClient: true});
 
-app.set("view-engine", "ejs");
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-  res.render("home.ejs");
+  res.render("home");
 });
 
 app.listen(process.env.PORT, function(){
