0ca88ee94d577ee92a510c035c0661d6b39134c6
Damian Reiter <damian.reiter@gmail.com>
2017-10-21 17:41:18 -0400

    file structure outline small app.js setup

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+var express = require('express'),
+    mongoose = require('mongoose'),
+    app = express();
 
-var express = require('express');
-var mongodb = require('mongodb');
-var app = express();
 var port = 3000;
+
+app.set("view-engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+  res.render("home.ejs");
+});
+
+app.listen(port, function(){
+  console.log("app started on port: " + port);
+});
